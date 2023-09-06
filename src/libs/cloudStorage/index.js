af3ff58358f23da4eb051c6eeaab42df9b4196d8
Yosif Hamed <josif.hameed@gmail.com>
2023-09-06 19:14:01 +0300

    Upload file to bucket

--- src/libs/cloudStorage/index.js
+++ src/libs/cloudStorage/index.js
@@ -1,14 +1,22 @@
 import { Storage } from '@google-cloud/storage'
-
+import path from 'path'
 // For more information on ways to initialize Storage, please see
 // https://googleapis.dev/nodejs/storage/latest/Storage.html
 
 // Creates a client from a Google service account key
 // const storage = new Storage({keyFilename: 'key.json'});
+const storage = new Storage()
+const bucketName = 'testing1watafak'
 
 export async function createBucket () {
-  const storage = new Storage()
-  const bucketName = 'testing1watafak'
   await storage.createBucket(bucketName)
   return `Bucket ${bucketName} created.`
+}
+
+export async function uploadFileToBucket () {
+  const filePath = path.resolve('./src/media/videos/tv-report.mp3')
+
+  await storage.bucket(bucketName).upload(filePath, { destination: filePath.split('/').pop() })
+
+  return `${filePath} uploaded to ${bucketName}`
 }
--- src/routes/cloudStorage.js
+++ src/routes/cloudStorage.js
@@ -1,8 +1,9 @@
-import { createBucket } from '../libs/cloudStorage/index.js'
+import { createBucket, uploadFileToBucket } from '../libs/cloudStorage/index.js'
 import express from 'express'
 
 const router = express.Router()
 
 router.get('/create-bucket', async (req, res) => res.send(await createBucket()))
+router.get('/upload-file', async (req, res) => res.send(await uploadFileToBucket()))
 
 export default router
